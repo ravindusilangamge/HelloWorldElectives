2499c919ba6d146e798909b0f12f365c65943799
ravindusilangamge <ravindusilangamge@gmail.com>
2024-04-13 20:32:24 +0530

    Changed search.tsx by sasanka

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,40 +1,58 @@
 'use client';
- 
+
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
-import { useDebouncedCallback } from 'use-debounce';
- 
+import { useState, useEffect } from 'react';
+
+// Debounce hook
+function useDebounce({value, delay}: { value: any, delay: any }) {
+    const [debouncedValue, setDebouncedValue] = useState(value);
+
+    useEffect(() => {
+        const handler = setTimeout(() => {
+            setDebouncedValue(value);
+        }, delay);
+
+        return () => {
+            clearTimeout(handler);
+        };
+    }, [value, delay]);
+
+    return debouncedValue;
+}
+
 export default function Search({ placeholder }: { placeholder: string }) {
-  const searchParams = useSearchParams();
-  const pathname = usePathname();
-  const { replace } = useRouter();
-  const handleSearch = useDebouncedCallback((term) => {
-  function handleSearch(term: string) {
-    console.log(`Searching... ${term}`);
-    const params = new URLSearchParams(searchParams);
-    if (term) {
-      params.set('query', term);
-    } else {
-      params.delete('query');
-    }
-    replace(`${pathname}?${params.toString()}`);
-    console.log(term);
-  }}, 300);
- 
-  return (
-    <div className="relative flex flex-1 flex-shrink-0">
-      <label htmlFor="search" className="sr-only">
-        Search
-      </label>
-      <input
-        className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-        placeholder={placeholder}
-        onChange={(e) => {
-          handleSearch(e.target.value);
-        }}
-        defaultValue={searchParams.get('query')?.toString()}
-      />
-      <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
-    </div>
-  );
+    const [input, setInput] = useState('');
+    const debouncedSearchTerm = useDebounce({value: input, delay: 300});
+    const searchParams = useSearchParams();
+    const pathname = usePathname();
+    const { replace } = useRouter();
+
+    useEffect(() => {
+        function handleSearch({term}: { term: any }) {
+            const params = new URLSearchParams(searchParams);
+            if (term) {
+                params.set('query', term);
+            } else {
+                params.delete('query');
+            }
+            replace(`${pathname}?${params.toString()}`);
+        }
+        console.log(debouncedSearchTerm);
+        handleSearch({term: debouncedSearchTerm});
+    }, [debouncedSearchTerm, pathname, replace, searchParams]);
+
+    return (
+        <div className="relative flex flex-1 flex-shrink-0">
+            <label htmlFor="search" className="sr-only">
+                Search
+            </label>
+            <input
+                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                placeholder={placeholder}
+                onChange={(e) => setInput(e.target.value)}
+            />
+            <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+        </div>
+    );
 }
