84bac5d6335b672a9189691151e46e3a841dfb72
ravindusilangamge <ravindusilangamge@gmail.com>
2024-04-16 11:46:39 +0530

    log in added

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,4 +1,4 @@
-import { GlobeAltIcon } from '@heroicons/react/24/outline';
+import { GlobeAltIcon, HeartIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
 
 export default function AcmeLogo() {
@@ -7,7 +7,8 @@ export default function AcmeLogo() {
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
+      {/* <HeartIcon className="h-12 w-12 rotate-[0deg]"/> */}
+      <p className="text-[44px]">Methsuwa</p>
     </div>
   );
 }
