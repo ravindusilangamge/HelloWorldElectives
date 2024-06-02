6223a297ab3f4afbe7f3db90e4a18fb8f5fe7774
ravindusilangamge <ravindusilangamge@gmail.com>
2024-06-02 15:25:22 +0530

    hellos

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -285,7 +285,7 @@ export async function updateInvoice(id: string, formData: FormData) {
       prescription: formData.get('prescription'),
     });
 
-    const dateReal = new Date();
+    const dateReal = new Date().toString();
     
     //const test = JSON.parse(prescription);
     let parsedPrescription = null;
